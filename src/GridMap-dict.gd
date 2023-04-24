155fe9c673e2adf11e024be4d4ca2ff6390cda38
David Liebman <radiodee1@gmail.com>
2023-04-24 15:59:19 -0400

    grid map dictionary -- define some prison shapes

--- src/GridMap-dict.gd
+++ src/GridMap-dict.gd
@@ -112,6 +112,31 @@ var game = {
 }
 
 
+var prison = {
+	'mesh': 1,
+	'layout': [
+		[],
+		[Vector2(1,0), Vector2(1,1), Vector2(1,2), Vector2(1,3), Vector2(1,4), Vector2(0,1), Vector2(0,3), Vector2(2,2)],
+		[Vector2(1,0), Vector2(1,1), Vector2(1,2), Vector2(1,3), Vector2(1,4), Vector2(2,1), Vector2(2,3), Vector2(0,2)],
+		[Vector2(0,1), Vector2(1,1), Vector2(2,1), Vector2(3,1), Vector2(4,1), Vector2(1,0), Vector2(3,0), Vector2(2,2)],
+		[Vector2(0,1), Vector2(1,1), Vector2(2,1), Vector2(3,1), Vector2(4,1), Vector2(1,2), Vector2(3,2), Vector2(2,0)]
+	],
+	'start': [
+		Vector2(0,0),
+		Vector2(1,0),
+		Vector2(1,0),
+		Vector2(0,1),
+		Vector2(0,1)
+	],
+	'end': [
+		Vector2(0,0),
+		Vector2(1,4),
+		Vector2(1,4),
+		Vector2(4,1),
+		Vector2(4,1)
+	]
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
