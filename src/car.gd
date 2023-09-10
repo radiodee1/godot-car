a8704a069da56e3c0bba08548e351b73c86217a2
David Liebman <radiodee1@gmail.com>
2023-09-10 15:21:43 -0400

    big wheels and other car script changes

--- src/car.gd
+++ src/car.gd
@@ -1,8 +1,19 @@
 extends VehicleBody3D
 
+var steer = 0
+var max_torque = 30
+var max_rpm = 50
+
+
 func _physics_process(delta):
 	pass
-	var steer = Input.get_axis("move_right", "move_left")
-	steering = steer * 1
+	steer = lerp(float(steer), float(Input.get_axis("move_right", "move_left") * 0.4), 5 * delta)
+	
+	steering = steer 
 	var acceleration = Input.get_axis("move_backward", "move_forward")
-	engine_force = acceleration * 100
+	var rpm = 0
+	rpm = abs($wheel_back_left.get_rpm())
+	$wheel_back_left.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	rpm = abs($wheel_back_right.get_rpm())
+	$wheel_back_right.engine_force = acceleration * max_torque * ( 1 - rpm / max_rpm)
+	
