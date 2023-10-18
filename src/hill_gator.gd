bc7c6e5a2f9594e5bc42eb21a013a69b8d05ad82
David Liebman <radiodee1@gmail.com>
2023-10-18 09:57:56 -0400

    try blender import

--- src/hill_gator.gd
+++ src/hill_gator.gd
@@ -1,8 +1,20 @@
 extends Node3D
 
+@onready var animation_player = $'gator_walk/AnimationPlayer'
+
+var anim_foot = 'footAction'
+var anim_foot_001 = 'foot_001Action'
+var anim_foot_002 = 'foot_002Action'
+var anim_foot_003 = 'foot_003Action'
+var anim_walk = 'walk'
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	animation_player.play(anim_foot)
+	#animation_player.play(anim_foot_001)
+	#animation_player.play(anim_foot_002)
+	#animation_player.play(anim_foot_003)
+	#animation_player.play(anim_walk)
 	pass # Replace with function body.
 
 
