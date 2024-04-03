708854fc836c8d0741c9ee1c8c8490d5dde1dda1
David Liebman <radiodee1@gmail.com>
2024-04-03 13:49:04 -0400

    ramp ver 2

--- src/ramp_old.gd
+++ src/ramp_old.gd
@@ -1,10 +1,10 @@
 extends StaticBody3D
 
 var table = {
-		Vector2(1, 0): deg_to_rad(0 + 90),
-		Vector2(0,-1): deg_to_rad(90 + 90),
-		Vector2(-1,0): deg_to_rad(180 + 90),
-		Vector2(0, 1): deg_to_rad(270 + 90)
+		Vector2(1, 0): deg_to_rad(90), # 270 
+		Vector2(0,-1): deg_to_rad(180), # 0 
+		Vector2(-1,0): deg_to_rad(270), # 90 
+		Vector2(0, 1): deg_to_rad(0) # 180 
 	}
 
 
@@ -22,7 +22,8 @@ func _process(delta):
 
 func init(v, map, location,  namex='RAMP', group='mob'):
 	var scale_local = 1 
-	v.y += -3.0 
+	v.y += -3.45
+	 
 	#var j = load("res://src/gator_walk.tscn").instantiate()
 	var loc = Vector2(location.x, location.z)
 
@@ -39,11 +40,12 @@ func init(v, map, location,  namex='RAMP', group='mob'):
 		return
 	v *= 0.5 	
 	#self.translate(v)
-	global_transform.origin = v 
+	
 
 	for i in table.keys():
 		var xx = map[loc.x + i.x][loc.y + i.y]
 		if xx == -1:
+			global_transform.origin = v 
 			rotate_y(table[i])
 			break
 
